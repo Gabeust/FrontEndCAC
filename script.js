c20c1d4f8eb9e5700388039c2b88990d4b23b374
Gabriel Romero <romero.gabriel.e@hotmail.com>
2023-10-12 13:29:16 -0300

    1

--- script.js
+++ script.js
@@ -1,19 +1,3 @@
-// Obtener la imagen y la ventana modal
-var img = document.getElementsByClassName('imagen');
-var modal = document.getElementById('myModal');
-var modalImg = document.getElementById("imgModal");
-
-// Cuando se hace clic en la imagen, abrir la ventana modal
-img.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-}
-
-// Cuando se hace clic en el botón de cerrar, cerrar la ventana modal
-var closeButton = document.getElementsByClassName("close")[0];
-closeButton.onclick = function() {
-    modal.style.display = "none";
-}
 
 function mostrarOcultar() {
     var article = document.querySelector('#sobre article');
@@ -27,3 +11,105 @@ function mostrarOcultar() {
         button.textContent = 'Leer más';
     }
 }
+
+
+// const productCardsContainer = document.querySelector("#productos-cards");
+
+
+// fetch('https://my-json-server.typicode.com/Gabeust/db.json/productos')
+//     .then(datos => datos.json())
+
+//     .then(data => {
+//         if (Array.isArray(data)) {
+//             // Procesar los datos de la API
+//             data.forEach(productos => {
+//                 // Crear una tarjeta para cada producto
+//                 const card = document.createElement("li");
+
+
+//                 // Crear el contenido de la tarjeta
+//                 card.innerHTML = `
+//         <div class="card-container">
+//             <div class="card">
+//                 <div class="card-img">
+//                     <img src="${productos.imagen_url}" >
+//                 </div>
+//                 <h4> ${productos.nombre} </h4>
+//                 <p> Cod. ${productos.codigo}</p>
+//             <div class="card-detalle">
+//                     <div class="precio">
+//                         <p>Precio <strong> $${productos.precio} </strong></p>
+//                     </div>
+//                 </div>
+//                 <button class="agregar">Agregar al Carrito</button>
+//             </div>
+//         </div>
+//         `;
+//                 // Agregar la tarjeta al contenedor
+//                 productCardsContainer.appendChild(card);
+
+//             })
+//         }
+//     })
+//     .catch(error => {
+//         console.error("Error al obtener los datos de la API:", error);
+//     });
+
+const productCardsContainer = document.querySelector("#productos-cards");
+let data; // Variable global para almacenar los datos del JSON
+
+fetch('https://my-json-server.typicode.com/Gabeust/db.json/productos')
+    .then(datos => datos.json())
+    .then(responseData => {
+        data = responseData; // Asignar los datos a la variable "data"
+        mostrarProductos(data); // Mostrar todos los productos al cargar la página
+    })
+    .catch(error => {
+        console.error("Error al obtener los datos de la API:", error);
+    });
+
+function mostrarProductos(productos) {
+    // Limpia el contenedor antes de mostrar los nuevos resultados
+    productCardsContainer.innerHTML = "";
+
+    productos.forEach(producto => {
+        const card = document.createElement("li");
+
+        card.innerHTML = `
+        <div class="card-container">
+            <div class="card">
+                <div class="card-img">
+                    <img src="${producto.imagen_url}" >
+                </div>
+                <h4>${producto.nombre}</h4>
+                <p>Cod. ${producto.codigo}</p>
+                <div class="card-detalle">
+                    <div class="precio">
+                        <p>Precio <strong> $${producto.precio}</strong></p>
+                    </div>
+                </div>
+                <button class="agregar">Agregar al Carrito</button>
+            </div>
+        </div>
+    `;
+        productCardsContainer.appendChild(card);
+    });
+}
+
+function mostrarProductosPorCategoria(categoria) {
+
+    const productosFiltrados = data.filter(producto => producto.categoria === categoria);
+    mostrarProductos(productosFiltrados);
+}
+
+
+const categoriasLinks = document.querySelectorAll("ul li a");
+
+categoriasLinks.forEach(link => {
+    link.addEventListener("click", (e) => {
+        e.preventDefault();
+        const categoriaSeleccionada = link.getAttribute("data-categoria");
+        mostrarProductosPorCategoria(categoriaSeleccionada);
+    });
+});
+
