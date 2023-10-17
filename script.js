f79d6cedf7190a18734a04ba1029b41ae234575f
Gabriel Romero <romero.gabriel.e@hotmail.com>
2023-10-17 17:38:51 -0300

    act. completa

--- script.js
+++ script.js
@@ -41,8 +41,10 @@ function mostrarProductos(productos) {
                 <div class="card-img">
                     <img src="${producto.imagen_url}" class="img-prod" >
                 </div>
+                <div class="detalle-prod">
                 <h4>${producto.nombre}</h4>
                 <p>Cod. ${producto.codigo}</p>
+                </div>
                 <div class="card-detalle">
                     <div class="precio">
                         <p>Precio <strong> $${producto.precio}</strong></p>
@@ -82,19 +84,20 @@ function validarFormulario() {
 
     // Verificar si los campos están vacíos
     if (!nombre || !email || !mensaje) {
-      mensajeError.textContent = 'Todos los campos son obligatorios';
-      return false;
+        mensajeError.textContent = 'Todos los campos son obligatorios';
+        return false;
     }
 
     // Verificar la validez del correo electrónico
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     if (!emailRegex.test(email)) {
-      mensajeError.textContent = 'Ingrese una dirección de correo electrónico válida';
-      return false;
+        mensajeError.textContent = 'Ingrese una dirección de correo electrónico válida';
+        return false;
     }
 
     return true;
 }
+
 
 // Giro de la imagen central
 let contenedorImagenCentral = document.getElementsByClassName("contenedor_imagen_central");
