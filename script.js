d981e573d97e2d778b21890f1058a774f3888809
mauricio <mtanusmafud@gmail.com>
2023-10-19 18:03:55 -0300

    Validación finalizada del formulario de suscripción

--- script.js
+++ script.js
@@ -25,20 +25,15 @@ var barraNavegacionVisiblePorBoton = false;
 var barraNavegacionVisiblePorAncho = false;
 
 function desplegarMenu() {
-    console.log("Barra de navegación es visible?", barraNavegacionVisiblePorBoton);
-    
     let barraNav = document.querySelector("#barra_navegacion");
 
     barraNav.style.display = barraNavegacionVisiblePorBoton ? "none" : "block";
     barraNavegacionVisiblePorBoton = !barraNavegacionVisiblePorBoton;
 
-    console.log(barraNav);
 }
 
 window.onresize = desplegarMenuSegunViewport;
 function desplegarMenuSegunViewport() {
-    console.log("Ancho del vieport en px:", window.innerWidth);
-    
     let barraNav = document.querySelector("#barra_navegacion");
     let anchoVentana = window.innerWidth;
 
@@ -53,8 +48,40 @@ function desplegarMenuSegunViewport() {
         }
     }
 
-    console.log(barraNav);
 } 
+
+function validarMailSuscripcion() {
+    let campoMail = document.querySelector("#email_suscripcion");
+    let mailIngresado = campoMail.value;
+
+    let expreg = /^[A-Za-z][\w.-]+\@[A-Za-z]+[.][A-Za-z]{2,}/i; // Inicie con letra, puede contener ., -, _ y luego del @ admite sólo letras
+
+    if (expreg.test(mailIngresado)) {
+        campoMail.classList.remove("campo_invalido")
+        campoMail.classList.add("campo_valido");
+        return true;
+    } else {
+        campoMail.classList.remove("campo_valido")
+        mailIngresado == "" ? campoMail.classList.remove("campo_invalido") : campoMail.classList.add("campo_invalido");
+        return false;
+    }
+
+}
+
+function validarFormularioSuscripcion() {
+
+    if (!validarMailSuscripcion()) {
+        alert("Ingrese un mail válido.\n\nNo está permitido que comience con números, no incluir ñ ni acentos, ni caracteres especiales.");
+        document.querySelector("#email_suscripcion").focus();
+    } else if (!document.querySelector("#aceptar_tyc").checked) {
+        alert("No olvide aceptar términos y condiciones de suscripción.");
+        document.querySelector("#aceptar_tyc").focus();
+    } else {
+        alert("Suscripción realizada con éxito!");
+        document.querySelector("#formulario_suscripcion").submit();
+    }
+
+}
 
 // Productos ------------- gabriel
 const productCardsContainer = document.querySelector("#productos-cards");
