9fdc2b747fb96e80e053a0aaf76801fe9cc92ddd
Gabriel Romero <romero.gabriel.e@hotmail.com>
2023-10-21 19:09:41 -0300

    actualizacion

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 // Sobre Nosotros -----------gabriel
 
 function mostrarOcultar() {
@@ -77,62 +76,6 @@ window.addEventListener("scroll", function () {
 
 
 // Productos ------------- gabriel
-// const productCardsContainer = document.querySelector("#productos-cards");
-// let data; // Variable global para almacenar los datos del JSON
-
-// fetch('https://my-json-server.typicode.com/Gabeust/db.json/productos')
-//     .then(datos => datos.json())
-//     .then(responseData => {
-//         data = responseData; // Asigna los datos a la variable "data"
-//         mostrarProductos(data); // Muestra todos los productos al cargar la página
-//     })
-//     .catch(error => {
-//         console.error("Error al obtener los datos de la API:", error);
-//     });
-
-// function mostrarProductos(productos) {
-//     // Limpia el contenedor antes de mostrar los nuevos resultados
-//     productCardsContainer.innerHTML = "";
-
-//     productos.forEach(producto => {
-//         const card = document.createElement("li");
-
-//         card.innerHTML = `
-//         <div class="card-container">
-//             <div class="card">
-//                 <div class="card-img">
-//                     <img src="${producto.imagen_url}" class="img-prod" >
-//                 </div>
-//                 <div class="detalle-prod">
-//                 <h4>${producto.nombre}</h4>
-//                 <p>Cod. ${producto.codigo}</p>
-//                 </div>
-//                 <div class="card-detalle">
-//                     <div class="precio">
-//                         <p>Precio <strong> $${producto.precio}</strong></p>
-//                     </div>
-//                 </div>
-//                 <button class="botones"> Agregar al Carrito</button>
-
-//             </div>
-//         </div>
-//     `;
-//         productCardsContainer.appendChild(card);
-//     });
-// }
-
-
-// const categoriasLinks = document.querySelectorAll("ul li a");
-
-// categoriasLinks.forEach(link => {
-//     link.addEventListener("click", (e) => {
-//         e.preventDefault();
-//         const categoriaSeleccionada = link.getAttribute("data-categoria");
-//         mostrarProductosPorCategoria(categoriaSeleccionada);
-//     });
-// });
-
-// ---------------------------------------------------------------
 const productCardsContainer = document.querySelector("#productos-cards");
 let data; // Variable global para almacenar los datos del JSON
 
@@ -154,27 +97,35 @@ function mostrarProductos(productos) {
         const card = document.createElement("li");
 
         card.innerHTML = `
-                <div class="card-container">
-                    <div class="card">
-                        <div class="card-img">
-                            <img src="${producto.imagen_url}" class="img-prod" >
-                        </div>
-                        <div class="detalle-prod">
-                            <h4>${producto.nombre}</h4>
-                            <p>Cod. ${producto.codigo}</p>
-                        </div>
-                        <div class="card-detalle">
-                            <div class="precio">
-                                <p>Precio <strong> $${producto.precio}</strong></p>
-                            </div>
-                        </div>
-                        <button class="botones" onclick="agregarAlCarrito('${producto.codigo}', '${producto.nombre}')"> Agregar al Carrito</button>
+        <div class="card-container">
+            <div class="card">
+                <div class="card-img">
+                    <img src="${producto.imagen_url}" class="img-prod" >
+                </div>
+                <div class="detalle-prod">
+                <h4>${producto.nombre}</h4>
+                <p>Cod. ${producto.codigo}</p>
+                </div>
+                <div class="card-detalle">
+                    <div class="precio">
+                        <p>Precio <strong> $${producto.precio}</strong></p>
                     </div>
                 </div>
-                `;
+                <button class="botones" data-product-id="${producto.codigo}">Agregar al Carrito</button>
+            </div>
+        </div>
+    `;
         productCardsContainer.appendChild(card);
     });
 }
+
+function mostrarProductosPorCategoria(categoria) {
+
+    const productosFiltrados = data.filter(producto => producto.categoria === categoria);
+    mostrarProductos(productosFiltrados);
+}
+
+
 const categoriasLinks = document.querySelectorAll("ul li a");
 
 categoriasLinks.forEach(link => {
@@ -184,17 +135,21 @@ categoriasLinks.forEach(link => {
         mostrarProductosPorCategoria(categoriaSeleccionada);
     });
 });
+//carrito de compra ------------------------------------------
 
-function agregarAlCarrito(codigo, nombre) {
-    // Obtener el carrito actual desde localStorage o crear uno nuevo
-    const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+const cartSection = document.getElementById('cart');
+const totalElement = document.getElementById('total');
 
-    // Buscar si el producto ya está en el carrito
-    const productoEnCarrito = carrito.find(producto => producto.codigo === codigo);
+function agregarAlCarrito(codigo) {
+    // Obtiene el carrito almacenado en localStorage
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
 
-    if (productoEnCarrito) {
-        // Si el producto ya está en el carrito, aumenta la cantidad
-        productoEnCarrito.cantidad++;
+    // Busca el producto en el carrito
+    const productoExistente = carrito.find(producto => producto.codigo === codigo);
+
+    if (productoExistente) {
+        // Si el producto ya está en el carrito, incrementa la cantidad
+        productoExistente.cantidad++;
     } else {
         // Si el producto no está en el carrito, agrégalo
         const producto = data.find(producto => producto.codigo === codigo);
@@ -203,10 +158,108 @@ function agregarAlCarrito(codigo, nombre) {
         }
     }
 
-    // Guardar el carrito actualizado en localStorage
+    // Guarda el carrito actualizado en localStorage
     localStorage.setItem('carrito', JSON.stringify(carrito));
 }
 
+// Escucha el evento "Agregar al Carrito"
+document.addEventListener('click', function (event) {
+    if (event.target && event.target.className === 'botones') {
+        const codigo = event.target.getAttribute('data-product-id');
+        agregarAlCarrito(codigo);
+    }
+});
+
+function actualizarCarrito() {
+    cartSection.innerHTML = '';
+    let totalGeneral = 0;
+
+    // Obtiene el carrito almacenado en localStorage
+    const carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+    carrito.forEach(producto => {
+        const article = document.createElement('article');
+        article.className = 'product';
+
+        const totalProducto = producto.precio * producto.cantidad;
+        totalGeneral += totalProducto;
+
+        article.innerHTML = `
+            <header>
+                <a class="remove" onclick="eliminarDelCarrito('${producto.codigo}')">
+                    <h3>Borrar Producto</h3>
+                    <div class="cart-img">
+                        <img src="${producto.imagen_url}"  style="width:55%">
+                    </div>
+                </a>
+            </header>
+            <div class="content">
+                <p>${producto.nombre}</p>
+            </div>
+            <div class="content">
+           
+            <h2 class="precio">$${producto.precio.toFixed(2)}</h2>
+            <div>
+                <span class="q-menos" onclick="restarCantidad('${producto.codigo}')">-</span>
+                <span class="q">${producto.cantidad}</span>
+                <span class="q-mas" onclick="sumarCantidad('${producto.codigo}')">+</span>
+                </div>
+                <h2 class="total-precio">$${totalProducto.toFixed(2)}</h2>
+            </div>
+        `;
+
+        cartSection.appendChild(article);
+    });
+
+    totalElement.textContent = totalGeneral.toFixed(2);
+}
+
+function eliminarDelCarrito(codigo) {
+    // Obtiene el carrito almacenado en localStorage
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+    // Elimina el producto del carrito
+    carrito = carrito.filter(producto => producto.codigo !== codigo);
+
+    // Actualiza el carrito en localStorage
+    localStorage.setItem('carrito', JSON.stringify(carrito));
+
+    actualizarCarrito();
+}
+
+function restarCantidad(codigo) {
+    // Obtiene el carrito almacenado en localStorage
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+    // Encuentra el producto en el carrito
+    const producto = carrito.find(p => p.codigo === codigo);
+
+    if (producto && producto.cantidad > 1) {
+        producto.cantidad--;
+        // Actualiza el carrito en localStorage
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+    }
+
+    actualizarCarrito();
+}
+
+function sumarCantidad(codigo) {
+    // Obtiene el carrito almacenado en localStorage
+    let carrito = JSON.parse(localStorage.getItem('carrito')) || [];
+
+    // Encuentra el producto en el carrito
+    const producto = carrito.find(p => p.codigo === codigo);
+
+    if (producto) {
+        producto.cantidad++;
+        // Actualiza el carrito en localStorage
+        localStorage.setItem('carrito', JSON.stringify(carrito));
+    }
+
+    actualizarCarrito();
+}
+
+actualizarCarrito(); // Actualiza el carrito al cargar la página
 
 // formulario -------------------------------------gabriel 
 
